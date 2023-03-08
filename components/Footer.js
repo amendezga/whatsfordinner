5ff3dca44f102ecab829df513aa2d93f51bd8bdb
Alejandro <amendez1361@bths.edu>
2023-03-07 19:36:11 -0500

    feat: adds footer component

---
+++ components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; 2023 Team 1, LLC</p>
+      <p><a href="/about">About</a></p>
+    </footer>
+  );
+}
+
+export default Footer;
