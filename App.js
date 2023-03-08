673091e3ca33bb58244553da39e4c4c7f4fe853f
Alejandro <amendez1361@bths.edu>
2023-03-07 19:42:24 -0500

    adds Nav and Footer to App.js

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Nav from './components/Nav.js';
+import Footer from './components/Footer.js';
+
+function App() {
+  return (
+    <div>
+      <Nav />
+      {/* Other content goes here */}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
