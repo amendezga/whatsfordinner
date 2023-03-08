f4e1b824384ba04a6f3d8442b0e52348686ec5e6
Amber Taveras <taveras.amber@gmail.com>
2023-03-07 21:37:02 -0500

    renders index of recipe data

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -1,7 +1,46 @@
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
 function Recipes (props) {
+
+    const [savedRecipes, setSavedRecipes] = useState(null);
+
+    const recipesURL = 'http://localhost:3001/recipes';
+
+    async function fetchSavedRecipes () {
+        try {
+            const response = await fetch(recipesURL);
+            const data = await response.json();
+            setSavedRecipes(data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        fetchSavedRecipes();
+    }, []);
+
+    function loaded () {
+        return savedRecipes.map((recipe) => {
+            return (
+                <div key={recipe._id}>
+                    <img src={recipe.image} alt={recipe.name} />
+                    <Link to={`recipes/${recipe._id}`}>
+                        <h1>{recipe.name}</h1>
+                    </Link>
+                </div>
+            );
+        });
+    }
+
+    function loading () {
+        return <h1>Loading...</h1>
+    }
+
     return (
-        <h1>this is the recipes components</h1>
-    );
+        savedRecipes ? loaded() : loading()
+        )       
 }
 
 
