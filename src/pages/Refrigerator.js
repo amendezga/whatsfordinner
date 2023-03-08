0a0da6e060f84f262780cc281c54ff97deea0baa
Alan.Chu <achu61@seas.upenn.edu>
2023-03-07 19:02:02 -0800

    added refrigerator pages and main component

--- src/pages/Refrigerator.js
+++ src/pages/Refrigerator.js
@@ -1,6 +1,13 @@
 function Refrigerator(props){
-    return(<>
-    Here is the Refrigerator!
-    </>)
-}
+    return <>
+        <h1>What we have in the refrigerator??</h1>
+        {props.refrigerator.map((ingredient) => (
+            <div className="ingredient">
+            <h6>{ingredient.name}</h6>
+            <button>Delete</button>
+        </div>
+        )
+)
+    }
+    <button>add moe Ingredients</button></>}
 export default Refrigerator;
